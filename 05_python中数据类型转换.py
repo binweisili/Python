d03c64884e82a421cc2f02037df8cef6277c973a
binweisili <1097865063@qq.com>
2025-03-16 18:54:37 +0800

    第二次提交

---
+++ 05_python中数据类型转换.py
@@ -0,0 +1,11 @@
+num_str = str(11)
+print(type(num_str), num_str)
+
+float_str = str(13.14)
+print(type(float_str), float_str)
+
+str_int = int("11")
+print(type(str_int), str_int)
+
+str_float = float(13.14)
+print(type(str_float), str_float)
